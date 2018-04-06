455f9e879ab42fff431c6e13c59bdf519854e8c9
youmebangbang <34688253+youmebangbang@users.noreply.github.com>
2018-04-06 15:19:13 -0400

    Add files via upload

---
+++ esp8266_motion_client.ino
@@ -0,0 +1,175 @@
+
+#include <ESP8266WiFi.h>
+
+#define Selector_0 16
+#define Selector_1 5
+#define Selector_2 4
+#define Enable_0 14
+#define Enable_1 12
+
+#define pot_limit 315
+#define timeout 10
+
+
+IPAddress serverIP(192, 168, 4, 1);
+
+
+//for ledcWrite
+//RANGE OF JX 2060 IS 1800 TO 7800
+//RANGE OF MG90S IS 1750 TO 7550
+
+//for adafruit PWM
+//RANGE OF MG90S IS 100 TO 500
+//RANGE OF JX60 IS 110 TO 460
+
+
+uint16_t pot_array[8];
+
+void select_channel(int);
+WiFiClient client;
+
+
+void setup() {
+  
+  pinMode(Selector_0, OUTPUT);
+  pinMode(Selector_1, OUTPUT);
+  pinMode(Selector_2, OUTPUT);
+  pinMode(Enable_0, OUTPUT);
+  pinMode(Enable_1, OUTPUT);
+
+  Serial.begin(115200);
+
+  client.setNoDelay(1);
+
+  const char *ssid = "espServer";
+  const char *password = "11111111";
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  while ( WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(WiFi.status());
+  }
+
+  Serial.println("WIFI connected");
+  Serial.println(WiFi.localIP());  // get interface IP address mask
+
+  Serial.println(client.connect(serverIP, 5000));
+}
+
+
+void loop() {
+
+  if (client.connected())
+  {
+    Serial.println("Connection success!");
+  }
+  else {
+    Serial.println("oops");
+    client.connect(serverIP, 5000);
+    delay(50);
+  }
+
+  for (;;)
+  {    
+    for (int channel = 0; channel < 8; channel++)
+    {
+      select_channel(channel);
+      pot_array[channel] = analogRead(0);
+
+      if (pot_array[channel] < pot_limit) {
+        pot_array[channel] = pot_limit;    
+      }     
+      
+      yield();
+      
+    }
+    
+    client.write_P((const char*)pot_array, sizeof(pot_array));
+    delay(timeout);
+    
+
+    if(!client.connected())
+    {
+      Serial.println("connection reset");
+      client.connect(serverIP, 5000);
+      break;
+    }
+    
+   // yield();
+  }
+
+}
+void select_channel(int channel_num)
+{
+  if (channel_num < 8)
+  {
+    digitalWrite(Enable_0, 0);
+    digitalWrite(Enable_1, 1);
+  }
+  else {
+    digitalWrite(Enable_0, 1);
+    digitalWrite(Enable_1, 0);
+  }
+
+  switch (channel_num) {
+
+    case 0:
+      digitalWrite(Selector_0, 0);
+      digitalWrite(Selector_1, 0);
+      digitalWrite(Selector_2, 0);
+      break;
+
+    case 1:
+      digitalWrite(Selector_0, 1);
+      digitalWrite(Selector_1, 0);
+      digitalWrite(Selector_2, 0);
+      break;
+
+
+    case 2:
+      digitalWrite(Selector_0, 0);
+      digitalWrite(Selector_1, 1);
+      digitalWrite(Selector_2, 0);
+      break;
+
+
+    case 3:
+      digitalWrite(Selector_0, 1);
+      digitalWrite(Selector_1, 1);
+      digitalWrite(Selector_2, 0);
+      break;
+
+
+    case 4:
+      digitalWrite(Selector_0, 0);
+      digitalWrite(Selector_1, 0);
+      digitalWrite(Selector_2, 1);
+      break;
+
+
+    case 5:
+      digitalWrite(Selector_0, 1);
+      digitalWrite(Selector_1, 0);
+      digitalWrite(Selector_2, 1);
+      break;
+
+
+    case 6:
+      digitalWrite(Selector_0, 0);
+      digitalWrite(Selector_1, 1);
+      digitalWrite(Selector_2, 1);
+      break;
+
+
+    case 7:
+      digitalWrite(Selector_0, 1);
+      digitalWrite(Selector_1, 1);
+      digitalWrite(Selector_2, 1);
+      break;
+  }
+}
---
+++ esp8266_motion_server.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define LOWFREQ 110
+#define HIFREQ 460
+
+//for ledcWrite
+//RANGE OF JX 2060 IS 1800 TO 7800
+//RANGE OF MG90S IS 1750 TO 7550
+
+//for adafruit PWM
+//RANGE OF MG90S IS 100 TO 500
+//RANGE OF JX60 IS 110 TO 460
+
+const char* ssid = "espServer";
+const char* password = "11111111";
+uint8_t readin[16];
+uint16_t pot_array[8];
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+uint8_t servonum = 0; // -1 correction
+
+int middle;
+
+WiFiServer server(5000);
+WiFiClient client;
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(5,4);
+  
+  delay(10);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+
+  server.begin();
+  server.setNoDelay(1);
+  IPAddress ServerIP = WiFi.softAPIP(); // Obtain the IP of the Server
+  Serial.print("Server IP is: "); // Print the IP to the monitor window
+  Serial.println(ServerIP);
+
+  middle = LOWFREQ + ((HIFREQ - LOWFREQ) / 2);
+  pwm.begin();
+  pwm.setPWMFreq(50);
+}
+
+void loop() {
+
+  // Check if a client has connected
+
+  if (!client.connected()) {
+    //Serial.println("no client yet");
+    client = server.available();
+    return;
+  }
+  Serial.println("GOT CLIENT!!!!!!!");
+  client.setNoDelay(1);
+
+  for (;;)
+  {
+
+    while (client.available())
+    {
+
+
+      client.read((uint8_t*)pot_array, sizeof(pot_array));
+
+      for(int x = 0; x < 8; x++)
+      {
+          pot_array[x] = map(pot_array[x], 315, 1024, LOWFREQ, HIFREQ); //10bit mapping
+
+          Serial.print(pot_array[x]);
+          Serial.print(',');
+          pwm.setPWM(x, 0, pot_array[x]);
+      }
+      Serial.println(" ");
+
+
+      yield();
+
+    }
+
+
+    if (!client.connected())
+    {
+      Serial.println("client disconnected");
+      //close client and reconnect
+      client.flush();
+      client.stop();
+      break;
+    }
+  }
+}
+
+
